aa5f491d4aa1aec029904ad0b3f519357be0cfdf
Manuel Herrera <52230895+wallyHack@users.noreply.github.com>
2022-02-21 23:45:59 -0600

    creamos el template login html, la redirección y ruta a login

--- bases/urls.py
+++ bases/urls.py
@@ -1,7 +1,10 @@
 
+from django import template
 from bases.views import Home
-from django.urls import path, include
+from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='bases/login.html'), name='login'),
 ]
--- syscofa/settings.py
+++ syscofa/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from telnetlib import LOGOUT
 
 from platformdirs import os
 
@@ -140,3 +141,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIECT_URL = '/login/'
