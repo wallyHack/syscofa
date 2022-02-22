2fddb7e101cb6dc00a972ae509d48ff67f5bed4b
Manuel Herrera <52230895+wallyHack@users.noreply.github.com>
2022-02-21 21:15:45 -0600

    home para usuarios autenticados usando mixin

--- bases/views.py
+++ bases/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic.base import TemplateView
 
-class Home(TemplateView):
+class Home(LoginRequiredMixin, TemplateView):    
     template_name = 'bases/home.html'
+    login_url = '/admin/' # sino esta logeado se redireccona al admin
