a255a9e8cbd7a455de58f251a83fabefe18eb4c7
Manuel Herrera <52230895+wallyHack@users.noreply.github.com>
2022-02-24 23:46:22 -0600

    logout y mensajes de error en la plantilla de login.html

--- bases/views.py
+++ bases/views.py
@@ -1,9 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic.base import TemplateView
 
 class Home(LoginRequiredMixin, TemplateView):    
     template_name = 'bases/home.html'
-    login_url = '/admin/' # sino esta logeado se redireccona al admin
+    login_url = 'bases:login' # sino esta logeado se redireccoona al admin
--- syscofa/settings.py
+++ syscofa/settings.py
@@ -137,10 +137,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIECT_URL = '/login/'
