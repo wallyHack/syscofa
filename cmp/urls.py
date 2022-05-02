b5cd5b6abe08ad66f1b2ed5270362d1eb985e187
Manuel Herrera <m.herreralara80@gmail.com>
2022-05-01 23:05:55 -0500

    plantilla modal(Popup) para inactivar un proveedor, rutas, vista y método ajax

--- cmp/urls.py
+++ cmp/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path, include
 
-from .views import ProveedorView, ProveedorNew, ProveedorEdit
+from .views import ProveedorView, ProveedorNew, ProveedorEdit, inactivar_proveedor
 
 urlpatterns = [
     path('proveedores/', ProveedorView.as_view(), name="proveedor_list"),
     path("proveedores/new", ProveedorNew.as_view(), name="proveedor_new"),
-    path("proveedores/edit:pk", ProveedorEdit.as_view(), name="proveedor_edit"),
+    path("proveedores/edit/<int:pk>", ProveedorEdit.as_view(), name="proveedor_edit"),
+    path("proveedores/inactivar/<int:id>", inactivar_proveedor, name="proveedor_inactivar"),
 ]
--- cmp/views.py
+++ cmp/views.py
@@ -1,8 +1,11 @@
+from re import template
 from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+import json
 
 from .models import Proveedor
 from .forms import ProveedorForm
@@ -37,3 +40,22 @@ class ProveedorEdit(LoginRequiredMixin, generic.UpdateView):
     def form_valid(self, form):
         form.instance.um = self.request.user.id #ubicamos al usuario que modificó el formulario
         return super().form_valid(form)
+
+def inactivar_proveedor(request, id):
+    template_name = "cmp/inactivar_proveedor.html"
+    contexto = {}
+
+    proveedor = Proveedor.objects.filter(pk=id).first()
+    if not proveedor:
+        return HttpResponse('Proveedor no existe' + str(id))
+
+    if request.method == 'GET':
+        contexto = {'obj': proveedor}
+
+    if request.method == 'POST':
+        proveedor.estado = False
+        proveedor.save()
+        contexto = {'obj':'OK'}
+        return HttpResponse('Proveedor Inactivado')
+
+    return render(request, template_name, contexto)
