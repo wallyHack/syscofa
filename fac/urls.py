14a4f1c9ae5985a2e0bad34fb3d8c936b8cab656
Manuel Herrera <m.herreralara80@gmail.com>
2022-07-02 05:32:14 -0500

    vistas y rutas para New Cliente y Edit Cliente

--- fac/urls.py
+++ fac/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path, include
-from .views import ClienteView
+from .views import ClienteView, ClienteNew, ClienteEdit
 
 urlpatterns = [
     path("clientes/", ClienteView.as_view(), name="cliente_list"),
+    path("clientes/new", ClienteNew.as_view(), name="cliente_new"),
+    path("clientes/edit/<int:pk>", ClienteEdit.as_view(), name="cliente_edit"),
 ]
--- fac/views.py
+++ fac/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
+from django.contrib.messages.views import SuccessMessageMixin
 from bases.views import Sin_Privilegios
 from .models import Cliente
+from .forms import ClienteForm
 
 # Create your views here.
 class ClienteView(Sin_Privilegios, generic.ListView):
@@ -10,3 +13,25 @@ class ClienteView(Sin_Privilegios, generic.ListView):
     template_name = "fac/cliente_list.html"
     context_object_name = "obj"
     permission_required = "fac/view_cliente"
+
+class ClienteNew(SuccessMessageMixin, Sin_Privilegios, generic.CreateView):
+    model = Cliente
+    template_name = "fac/cliente_form.html"
+    context_object_name = "obj"
+    form_class = ClienteForm
+    success_url = reverse_lazy('fac:cliente_list')
+    permission_required = 'fac.add_cliente'
+    success_message = "Cliente creado satisfactoriamente"
+
+    def form_valid(self, form):
+        form.instance.uc = self.request.user #ubicamos al usuario que creo el formulario
+        return super().form_valid(form)
+
+class ClienteEdit(SuccessMessageMixin, Sin_Privilegios, generic.UpdateView):
+    model = Cliente
+    template_name = "fac/cliente_form.html"
+    context_object_name = "obj"
+    form_class = ClienteForm
+    success_url = reverse_lazy("fac:cliente_list") 
+    permission_required = 'fac.change_cliente'
+    success_message = "Cliente actualizado satisfactoriamente"
