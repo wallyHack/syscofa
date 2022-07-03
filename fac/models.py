3a18613e61c48874c88673673ac563f8838febb5
Manuel Herrera <m.herreralara80@gmail.com>
2022-07-03 00:06:18 -0500

    modelos encabezado y detalle para facturación

--- fac/models.py
+++ fac/models.py
@@ -1,9 +1,13 @@
+from tabnanny import verbose
+from tkinter import CASCADE
 from django.db import models
 
-from bases.models import ClaseModelo
+from bases.models import ClaseModelo, ClaseModelo2
+from inv.models import Producto
 
 # Create your models here.
 class Cliente(ClaseModelo):
+    """ modelo que representa un cliente """
     #example of choices
     NAT = 'Natural'
     JUR = 'Jurídica'
@@ -41,3 +45,44 @@ class Cliente(ClaseModelo):
 
     class Meta:
         verbose_name_plural = 'Clientes'
+
+class FacturaEnc(ClaseModelo2):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    fecha = models.DateTimeField(auto_now_add=True)
+    sub_total = models.FloatField(default=0)
+    descuento = models.FloatField(default=0)
+    total = models.FloatField(default=0)
+
+    def __str__(self):
+        return '{}'.format('id')
+
+    def save(self):
+        self.total = self.sub_total - self.descuento
+        super(FacturaEnc, self).save()
+
+    class Meta:
+        verbose_name_plural = "Encabezado Facturas"
+        verbose_name = "Encabezado Factura"
+
+class FacturaDet(ClaseModelo2):
+    factura = models.ForeignKey(FacturaEnc, on_delete=models.CASCADE)
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    cantidad = models.BigIntegerField(default=0)
+    precio = models.FloatField(default=0)
+    sub_total = models.FloatField(default=0)
+    descuento = models.FloatField(default=0)
+    total = models.FloatField(default=0)
+
+    def __str__(self):
+        return '{}'.format(self.producto)
+
+    def save(self):
+        self.sub_total = float(float(int(self.cantidad)) * float(self.precio))
+        self.total = self.sub_total - float(self.descuento)
+        super(FacturaDet, self).save()
+
+    class Meta:
+        verbose_name_plural = "Detalles Facturas"
+        verbose_name = "Detalle Factura"
+
+
